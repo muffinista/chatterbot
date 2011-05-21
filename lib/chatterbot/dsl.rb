3dec949bf30da6c8d71d7b3636b4fd54aa32b416
Colin Mitchell <colin@muffinlabs.com>
2011-05-20 20:59:53 -0400

    removing unused docs

--- lib/chatterbot/dsl.rb
+++ lib/chatterbot/dsl.rb
@@ -1,39 +1,5 @@
 require File.join(File.dirname(__FILE__), '..', 'chatterbot')
 
-#require 'rdoc/usage'
-
-# == Synopsis 
-#   This is a sample description of the application.
-#   Blah blah blah.
-#
-# == Examples
-#   This command does blah blah blah.
-#     ruby_cl_skeleton foo.txt
-#
-#   Other examples:
-#     ruby_cl_skeleton -q bar.doc
-#     ruby_cl_skeleton --verbose foo.html
-#
-# == Usage 
-#   ruby_cl_skeleton [options] source_file
-#
-#   For help use: ruby_cl_skeleton -h
-#
-# == Options
-#   -h, --help          Displays help message
-#   -v, --version       Display the version, then exit
-#   -q, --quiet         Output as little as possible, overrides verbose
-#   -V, --verbose       Verbose output
-#   TO DO - add additional options
-#
-# == Author
-#   YourName
-#
-# == Copyright
-#   Copyright (c) 2007 YourName. Licensed under the MIT License:
-#   http://www.opensource.org/licenses/mit-license.php
-
-
 module Chatterbot
   #
   # very basic DSL to handle the common stuff you would want to do with a bot.
