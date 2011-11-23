74ce6607f9af06bf1cd2da80e65d5c26d4f35e6e
Colin Mitchell <muffinista@gmail.com>
2011-11-22 20:01:28 -0500

    use #replace_variables in example bot

--- examples/echoes_bot.rb
+++ examples/echoes_bot.rb
@@ -21,7 +21,12 @@ puts "checking for replies to me"
 replies do |tweet|
   
   # replace the incoming username with the handle of the user who tweeted us
-  src = tweet[:text].gsub(/@echoes_bot/, tweet_user(tweet))
+  # src = tweet[:text].gsub(/@echoes_bot/, tweet_user(tweet))
+
+  # replace the incoming username with #USER#, which will be replaced
+  # with the handle of the user who tweeted us by the
+  # replace_variables helper
+  src = tweet[:text].gsub(/@echoes_bot/, "#USER#")  
 
   # send it back!
   reply src, tweet
