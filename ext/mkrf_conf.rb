c03fd1aa6413b03ea7ebe7239f4a37a2d36ee3b1
Colin Mitchell <muffinista@gmail.com>
2017-04-25 20:56:35 -0400

    Fix activesupport installation

--- ext/mkrf_conf.rb
+++ ext/mkrf_conf.rb
@@ -18,3 +18,11 @@ rescue => e
 
   exit!
 end
+
+
+# https://en.wikibooks.org/wiki/Ruby_Programming/RubyGems#How_to_install_different_versions_of_gems_depending_on_which_version_of_ruby_the_installee_is_using
+
+# create dummy rakefile to indicate success
+f = File.open(File.join(File.dirname(__FILE__), "Rakefile"), "w")
+f.write("task :default\n")
+f.close
